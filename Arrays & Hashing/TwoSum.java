2918d2a310567c09a4714589d29857bed921a2ce
Lakshmi <lakshmi.kottapallis@gmail.com>
2025-07-13 20:10:43 -0400

    TwoSum

---
+++ Arrays & Hashing/TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+//Create a Integer Hashmap and check to see if the difference (target - nums[i[) exists in Hashmap
+//First calculate difference, check if map contains, THEN put in hashmap
+//Key is value, then index because we are comparing with key and obtain/return the index 
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (map.containsKey(difference) == true) {
+                return new int[]{map.get(difference), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{} ;
+    }
+}
+
